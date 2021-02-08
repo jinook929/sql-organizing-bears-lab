a7852d0c24df10db33ec531a3a2684f7ea777197
jinook929 <jinook929@gmail.com>
2021-02-08 12:19:51 -0500

    commit tree 4d320f85a84b2924a56496e925bc2b574c92ed50

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "Mr. Chocolate", 6, "M", "brown", "calm", 1);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, "Rowdy", 4, "M", "grey", "mild", 1);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, "Tabitha", 7, "F", "brown", "nice", 1);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, "Sergeant Brown", 23, "M", "black", "goofy", 0);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, "Melissa", 13, "F", "dark brown", "intense", 1);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, "Grinch", 8, "M", "dark brown", "grumpy", 0);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, "Wendy", 15, "F", "brown", "happy", 0);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 20, "M", "black", "aggressive", 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, "Mr. Chocolate", 6, "M", "brown", "calm", 1),
+(2, "Rowdy", 4, "M", "grey", "mild", 1),
+(3, "Tabitha", 7, "F", "brown", "nice", 1),
+(4, "Sergeant Brown", 23, "M", "black", "goofy", 0),
+(5, "Melissa", 13, "F", "dark brown", "intense", 1),
+(6, "Grinch", 8, "M", "dark brown", "grumpy", 0),
+(7, "Wendy", 15, "F", "brown", "happy", 0),
+(8, NULL, 20, "M", "black", "aggressive", 1);
